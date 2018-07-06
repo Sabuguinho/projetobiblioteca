7505eb4e387765412bc421986b99aeedcca4f4f6
LucasDallo <32512715+LucasDallo@users.noreply.github.com>
2018-07-06 20:59:58 -0300

    trabalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { CadastroAluno } from './pages/cadastroaluno/cadastro-aluno';
 import { CadastroLivro } from './pages/livro/cadastro-livro';
+import { Login } from 'src/app/pages/login/login';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgModule } from '@angular/core';
+import {MatButtonModule, MatCheckboxModule,MatSliderModule,
+  MatButtonToggle,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatListModule,
+  MatSidenavModule,
+  MatCardModule,
+  MatTabsModule
+} from '@angular/material';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { AppComponent } from './app.component';
+import {MatInputModule} from '@angular/material/input';
+import { FlexLayoutModule} from '@angular/flex-layout';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { AppRouter} from './app-router';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { CadastroCliente } from './pages/cadastrarcliente/cadastro-cliente';
+import { CadastroUsuario } from './pages/cadastrarusuario/cadastro-usuario';
+import { AlterarSenha } from './pages/alterarsenha/alterar-senha';
+import { Menu } from './componentes/menu/menu';
 
 @NgModule({
   declarations: [
-    AppComponent, CadastroLivro
+    AppComponent, CadastroLivro,CadastroAluno,Login,Menu
   ],
   imports: [
-    BrowserModule
+    BrowserModule,        MatExpansionModule,
+    MatListModule,
+    MatSidenavModule,
+    AppRouter,FlexLayoutModule,
+    MatCardModule,BrowserAnimationsModule,
+    BrowserModule,ReactiveFormsModule,
+    MatDatepickerModule, MatNativeDateModule,
+    MatButtonModule, MatCheckboxModule, MatSelectModule,
+    MatAutocompleteModule, MatInputModule, MatFormFieldModule,
+    FormsModule,MatSliderModule,MatTabsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
